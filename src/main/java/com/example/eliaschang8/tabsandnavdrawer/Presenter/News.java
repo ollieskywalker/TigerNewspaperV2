bd37edaa331214b8257eb7a686daf1acd053c6eb
Oliver Chang <ollieskywalker@gmail.com>
2017-05-15 12:49:10 -0700

    AlmostDoneWithMain

---
+++ src/main/java/com/example/eliaschang8/tabsandnavdrawer/Presenter/News.java
@@ -0,0 +1,31 @@
+package com.example.eliaschang8.tabsandnavdrawer.Presenter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.eliaschang8.tabsandnavdrawer.R;
+
+/**
+ * Created by eliaschang8 on 4/26/17.
+ */
+
+public class News extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
